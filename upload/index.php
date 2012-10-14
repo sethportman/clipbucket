1cd78543e92e4aeb90de8000b2c75aabf9194647
sethportman <sethportman16@gmail.com>
2012-10-14 18:38:32 -0500

    2

--- upload/index.php
+++ upload/index.php
@@ -1,25 +1,5 @@
 <?php
-/* 
- ***************************************************************
- | Copyright (c) 2007-2010 Clip-Bucket.com. All rights reserved.
- | @ Author	   : ArslanHassan									
- | @ Software  : ClipBucket , © PHPBucket.com					
- ****************************************************************
-*/
-
-define('THIS_PAGE','index');
-require 'includes/config.inc.php';
-
-$pages->page_redir();
-
-if(is_installed('editorspick'))
-{
-	assign('editor_picks',get_ep_videos());
-}
-
-//i love coding :)
-
 //Displaying The Template
-template_files('index.html');
+template_files('google55a495b60598a643.html');
 display_it();
 ?>
